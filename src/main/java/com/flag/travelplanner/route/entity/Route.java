d7b3a97066bb19aeb7542e7456602d717923fe72
Fukun Lai <laifukun@gmail.com>
2021-06-26 15:25:36 -0500

    implement POI service, route service

--- src/main/java/com/flag/travelplanner/route/entity/Route.java
+++ src/main/java/com/flag/travelplanner/route/entity/Route.java
@@ -1,12 +1,11 @@
 package com.flag.travelplanner.route.entity;
 
 import com.flag.travelplanner.poi.entity.POI;
+import com.flag.travelplanner.user.entity.User;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.sql.Date;
 import java.util.List;
 
 @Entity
@@ -16,7 +15,78 @@ public class Route implements Serializable {
     private static final long serialVersionUID = 670983485351766613L;
     @Id
     private long routeId;
+    private String name;
+    private Date createTime;
+    private String startAddress;
+    private String endAddress;
 
-   // @ManyToMany
-   // private List<POI> poiList;
+    @OneToMany
+    private List<POI> poiList;
+
+    @ManyToOne
+    private User user;
+
+    public Route(long routeId, String name, Date createTime, String startAddress, String endAddress) {
+        this.routeId = routeId;
+        this.name = name;
+        this.createTime = createTime;
+        this.startAddress = startAddress;
+        this.endAddress = endAddress;
+    }
+
+    public long getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(long routeId) {
+        this.routeId = routeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStartAddress() {
+        return startAddress;
+    }
+
+    public void setStartAddress(String startAddress) {
+        this.startAddress = startAddress;
+    }
+
+    public String getEndAddress() {
+        return endAddress;
+    }
+
+    public void setEndAddress(String endAddress) {
+        this.endAddress = endAddress;
+    }
+
+    public List<POI> getPoiList() {
+        return poiList;
+    }
+
+    public void setPoiList(List<POI> poiList) {
+        this.poiList = poiList;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/flag/travelplanner/route/service/RouteService.java
+++ src/main/java/com/flag/travelplanner/route/service/RouteService.java
@@ -1,18 +1,17 @@
 package com.flag.travelplanner.route.service;
 
-import com.flag.travelplanner.poi.entity.POI;
 import com.flag.travelplanner.route.entity.Route;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public interface RouteService {
 
-    List<POI> retrieveRoute(long id);
+    Route retrieveRoute(long id);
     void updateRoute(Route route);
     void deleteRoute(long id);
-    void saveRoute(Route route);
+    Route saveRoute(Route route);
 }
 
 // RouteService interface to be implemented
