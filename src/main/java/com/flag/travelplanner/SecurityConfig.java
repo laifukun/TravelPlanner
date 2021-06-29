56f439eae0a3e7088a70620bf69601a9528a2337
Fukun Lai <laifukun@gmail.com>
2021-06-28 23:14:40 -0500

    logout

--- src/main/java/com/flag/travelplanner/SecurityConfig.java
+++ src/main/java/com/flag/travelplanner/SecurityConfig.java
@@ -8,11 +8,25 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -32,19 +46,53 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     }
 
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+
+      //  http.cors();
+        http
+                .cors()
+                .configurationSource(request -> {
+                    var cors = new CorsConfiguration();
+                    cors.setAllowedOrigins(List.of("http://localhost:3000"));
+                    cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+                    cors.setAllowedHeaders(List.of("*"));
+                    return cors;
+                })
+                .and()
+                .authorizeRequests()
+                .antMatchers("/routes", "/routes/*")
+                .hasAuthority("ROLE_USER")
+                .anyRequest().permitAll()
+                .and()
+
                 .formLogin()
                 //.loginPage("/users/login")
                  .loginProcessingUrl("/users/login")
-                .failureForwardUrl("/users/login?error=true");
-        http
-                .authorizeRequests()
-                .antMatchers("/routes", "/routes/*")
-                    .hasAuthority("ROLE_USER")
-                .anyRequest().permitAll();
+                .failureUrl("/users/login?error=true")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication) throws IOException, ServletException {
+                    //do nothing
+                    }
+                })
+                .and()
+                .logout()
+                .logoutUrl("/users/logout")
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication) throws IOException, ServletException {
+                        //do nothing
+                    }
+                })
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .and()
+                .csrf().disable();
 
         // http builder configurations for authorize requests and form login (see below)
     }
